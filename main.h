40203f3e7e5d688a34137eb0c0b00124905f0f5a
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-07 10:11:38 -0700

    created main.h file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+
+#endif
